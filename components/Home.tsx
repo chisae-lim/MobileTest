d61a56974e018282e7fab81adbac3c6c6c7a51c3
Chisae <chisae0123@gmail.com>
2024-12-15 11:22:44 +0700

    Firebase Notification

---
+++ components/Home.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function Home(): React.JSX.Element {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+
+}
+
+export default Home;
---
+++ components/Setting.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function Setting(): React.JSX.Element {
+    return (
+        <View>
+            <Text>Setting</Text>
+        </View>
+    );
+
+}
+
+export default Setting;
